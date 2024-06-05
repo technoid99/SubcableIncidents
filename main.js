0df9b21c2d39910a495c769eddec86877f38530f
technoid99 <59440183+technoid99@users.noreply.github.com>
2024-06-06 00:21:27 +1000

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let filteredData = []; //created global only so can share the results as CSV thr
 async function fetchCSV() {
     
     //const response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQwM2mfSIQK_-lAYzBvFQ-EX0Jq3XfwHETZkpY8I4pahueBF28Z4j6m_d1k2_Z3tcXdw2VhpvenZNbO/pub?gid=0&single=true&output=csv');
-    const response = await fetch('cable_incidents.csv');
+    const response = await fetch('https://technoid99.github.io/SubcableIncidents/cable_incidents.csv');
     const csvText = await response.text();
     const parsedData = Papa.parse(csvText, { header: true }).data;
     originalData = parsedData; // Store original data
